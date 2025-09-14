8b7b8d2e1ad2484e0a4bf6ca3c9c301f08a944ab
awesomemin <saogig0503@naver.com>
2025-09-14 17:44:46 +0900

    deleted every code about auth

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
-  app.useGlobalPipes(new ValidationPipe());
 
   const port = configService.get<number>('PORT');
   if (!port) {
